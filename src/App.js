88a48cbf250842c29548b1fbd0c1284c5275829a
dtskalyan <kalyan.datta@dreamztech.com>
2022-08-16 15:24:26 +0530

    after third changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
        checking react app with cicd
        <h1>another code push</h1>
+       <p>after cicd integrate with buddy</p>
       </header>
     </div>
   );
