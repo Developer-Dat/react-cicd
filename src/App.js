8cc810fdc74e3425c2012dd0994eb1f221a35d5c
dtskalyan <kalyan.datta@dreamztech.com>
2022-08-16 13:08:47 +0530

    second changes done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        checking react app with cicd
+       <h1>another code push</h1>
       </header>
     </div>
   );
